d3d45116e58471584101c44e77e8c639208f7706
Fatma Nur <146754525+fnyzc@users.noreply.github.com>
2025-12-08 22:20:33 +0300

    Update MainMenu_sc.cs

--- hafta8/MainMenu/MainMenu_sc.cs
+++ hafta8/MainMenu/MainMenu_sc.cs
@@ -1,1 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu_sc : MonoBehaviour
+{
+ public void LoadGame()
+    {
+        SceneManager.LoadScene(0);
+    }  
+}
 
