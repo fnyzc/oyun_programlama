701dfdf9e2eaa32653d503ed0a81e16199535b88
Fatma Nur <146754525+fnyzc@users.noreply.github.com>
2026-01-05 22:56:41 +0300

    Add files via upload

---
+++ hafta12/ANN_sc.cs
@@ -0,0 +1,138 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ANN_sc 
+{
+   public int numberOfInputs;
+   public int numberOfOutputs;
+   public int numberOfHiddenLayers;
+   public int numberOfNeuronsPerHiddenLayer;
+
+   public double alpha;
+
+   List<Layer_sc> layers = new List<Layer_sc>();
+
+
+   public ANN_sc(int numberOfInputs, int numberOfOutputs, int numberOfHiddenLayers, int numberOfNeuronsPerHiddenLayer, double alpha)
+    {
+        this.numberOfInputs = numberOfInputs;
+        this.numberOfOutputs = numberOfOutputs;
+        this.numberOfHiddenLayers = numberOfHiddenLayers;
+        this.numberOfNeuronsPerHiddenLayer= numberOfNeuronsPerHiddenLayer;
+        this.alpha = alpha;
+
+        if (numberOfHiddenLayers > 0)
+        {
+            layers.Add(new Layer_sc(numberOfNeuronsPerHiddenLayer,numberOfInputs));
+
+            for(int i=0; i< numberOfHiddenLayers-1; i++)
+            {
+                layers.Add(new Layer_sc(numberOfNeuronsPerHiddenLayer, numberOfNeuronsPerHiddenLayer));
+
+            }
+            layers.Add(new Layer_sc(numberOfOutputs, numberOfNeuronsPerHiddenLayer));
+
+        }
+        else
+        {
+            layers.Add(new Layer_sc(numberOfOutputs, numberOfInputs));
+        }
+    }
+
+    public List<double> Run(List<double> inputValues, List<double> desiredOutput)
+    {
+        List<double> inputs = new List<double>();
+        List<double> outputs = new List<double>();
+
+        if(inputValues.Count != numberOfInputs)
+        {
+            Debug.Log("ERROR: Number of Inputs must be " + numberOfInputs);
+            return outputs;
+        }
+        inputs = new List<double>(inputValues);
+        for(int i=0; i<numberOfHiddenLayers+1; i++)
+        {
+            if (i > 0)
+            {
+                inputs = new List<double>(outputs);
+            }
+            outputs.Clear();
+
+            for(int j=0; j<layers[i].numberOfNeurons; j++)
+            {
+                double N=0;
+                layers[i].neurons[j].inputs.Clear();
+
+                for(int k=0; k< layers[i].neurons[j].numberOfInputs; k++)
+                {
+                    layers[i].neurons[j].inputs.Add(inputs[k]);
+                    N += layers[i].neurons[j].weights[k] * inputs[k];
+                }
+
+                N -= layers[i].neurons[j].bias;
+                layers[i].neurons[j].output = ActivationFunction(N);
+                outputs.Add(layers[i].neurons[j].output);
+            }
+        }
+        UpdateWeights(outputs, desiredOutput);
+        return outputs;
+    }
+
+    private double ActivationFunction(double value)
+    {
+        return Sigmoid(value);
+    }
+
+    double Step(double value)
+    {
+        if(value<0) return 0;
+        else return 1;
+    }
+
+    private double Sigmoid(double value)
+    {
+        double k = (double) System.Math.Exp(value);
+        return k/ (1.0f +k);
+    }
+
+    private void UpdateWeights(List<double> outputs, List<double> desiredOutput)
+    {
+        double error;
+        for(int i = numberOfHiddenLayers; i>=0; i--)
+        {
+            for(int j=0; j< layers[i].numberOfNeurons; j++)
+            {
+                if(i== numberOfHiddenLayers)
+                {
+                    error = desiredOutput[j] - outputs[j];
+                     layers[i].neurons[j].errorGradient = outputs[j] * (1- outputs[j]) * error;
+
+                }
+                else
+                {
+                     layers[i].neurons[j].errorGradient =  layers[i].neurons[j].output * (1-layers[i].neurons[j].output);
+                     double errorGradSum=0;
+                     for(int p=0; p<layers[i+1].numberOfNeurons; p++)
+                    {
+                        errorGradSum += layers[i+1].neurons[p].errorGradient * layers[i+1].neurons[p].weights[j];
+                    }
+                    layers[i].neurons[j].errorGradient *= errorGradSum;
+                }
+                for(int k=0; k< layers[i].neurons[j].numberOfInputs; k++)
+                {
+                    if(i== numberOfHiddenLayers)
+                    {
+                        error = desiredOutput[j] - outputs[j];
+                        layers[i].neurons[j].weights[k] += alpha * layers[i].neurons[j].inputs[k] * error;
+                    }
+                    else
+                    {
+                        layers[i].neurons[j].weights[k] += alpha * layers[i].neurons[j].inputs[k] * layers[i].neurons[j].errorGradient;
+                    }
+                }
+                layers[i].neurons[j].bias += alpha * -1 * layers[i].neurons[j].errorGradient;
+            }
+        }
+    }
+}
